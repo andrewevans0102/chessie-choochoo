3ddc15f6c0ef25b8602ef6f8363fe96ab937d0d0
Andrew Evans <andrewevans0102@gmail.com>
2019-10-14 14:03:12 -0400

    fixing failing unit tests

--- src/app/map-page/map-page.component.spec.ts
+++ src/app/map-page/map-page.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MapPageComponent } from './map-page.component';
+import { HeaderComponent } from '../header/header.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('MapPageComponent', () => {
   let component: MapPageComponent;
@@ -8,9 +10,9 @@ describe('MapPageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MapPageComponent ]
-    })
-    .compileComponents();
+      declarations: [MapPageComponent, HeaderComponent],
+      imports: [RouterTestingModule]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
